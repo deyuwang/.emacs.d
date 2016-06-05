832942291e81bab94061947cd1c60832eba72998
wdy <wdy@126.com>
2016-06-05 12:08:30 +0800

    add init-ibuffer.el

--- init.el
+++ init.el
@@ -16,10 +16,13 @@
 
 (package-initialize)
 
+
+(if (eq system-type 'windows-nt)
+	(require 'init-windows-nt)
+  	(require 'init-linux))
+
 (require 'init-basic)
 (require 'init-text)
-
-; 3rd lib
 (require 'init-theme)
 (require 'init-auto-complete)
 (require 'init-yasnippet)
@@ -27,23 +30,14 @@
 (require 'init-window-numbering)
 (require 'init-smex)
 (require 'init-youdao-dictionary)
-
 (require 'ox-freemind)
-
-(if (eq system-type 'windows-nt)
-	(require 'init-windows-nt)
-  	(require 'init-linux))
-
 (require 'init-git)
-
 ;; (require 'init-speedbar)
 ;; (require 'init-hide-region)
 ;; (require 'init-hide-lines)
 ;; (require 'init-highlight-parentheses)
-
 (require 'wdy-funs)
+(require 'init-ibuffer)
 
-;; 使用ibuffer替代默认的buffer列表
-(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (provide 'init)
