a8b904333277ae703b743d8e9b39f706ad67e9bd
wdy <wdy@123.com>
2017-04-13 15:53:32 +0800

    update org-mode funcs

---
+++ lisp/.#wdy-funs.el
@@ -0,0 +1,1 @@
+neo@neosMBP.local.617
--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -1,45 +1,49 @@
 
 (require 'org)
 
-(setq org-publish-project-alist
-	  '(("org-html"
-		  :base-directory "~/org/"
-		  :publishing-directory "~/public_html"
-		  :publishing-function org-html-publish-to-html
-		  :html-head "<link rel=\"stylesheet\" href=\"./style/style.css\" type=\"text/css\"/>")))
+;; 中文换行问题  
+(add-hook 'org-mode-hook   
+      (lambda () (setq truncate-lines nil)))
 
-(setq org-publish-project-alist
-	  '(("org"
-		  :base-directory "~/org/"
-		  :publishing-directory "~/public_html"
-		  :publishing-function org-latex-publish-to-pdf
-		  :section-numbers nil
-		  :with-toc nil)))
+;; (setq org-publish-project-alist
+;; 	  '(("org-html"
+;; 		  :base-directory "~/org/"
+;; 		  :publishing-directory "~/public_html"
+;; 		  :publishing-function org-html-publish-to-html
+;; 		  :html-head "<link rel=\"stylesheet\" href=\"./style/style.css\" type=\"text/css\"/>")))
+
+;; (setq org-publish-project-alist
+;; 	  '(("org"
+;; 		  :base-directory "~/org/"
+;; 		  :publishing-directory "~/public_html"
+;; 		  :publishing-function org-latex-publish-to-pdf
+;; 		  :section-numbers nil
+;; 		  :with-toc nil)))
 
 
-(setq org-publish-project-alist
-	  '(("orgfiles"
-		 :base-directory "~/org/"
-		 :base-extension "org"
-		 :publishing-directory "/ssh:user@host:~/html/notebook/"
-		 :publishing-function org-html-publish-to-html
-		 :exclude "PrivatePage.org" ;; regexp
-		 :headline-levels 3
-		 :section-numbers nil
-		 :with-toc nil
-		 :html-head "<link rel=\"stylesheet\"href=\"../other/mystyle.css\" type=\"text/css\"/>"
-		 :html-preamble t)
-		("images"
-		 :base-directory "~/images/"
-		 :base-extension "jpg\\|gif\\|png"
-		 :publishing-directory "/ssh:user@host:~/html/images/"
-		 :publishing-function org-publish-attachment)
-		("other"
-		 :base-directory "~/other/"
-		 :base-extension "css\\|el"
-		 :publishing-directory "/ssh:user@host:~/html/other/"
-		 :publishing-function org-publish-attachment)
-		("website" :components ("orgfiles" "images" "other"))))
+;; (setq org-publish-project-alist
+;; 	  '(("orgfiles"
+;; 		 :base-directory "~/org/"
+;; 		 :base-extension "org"
+;; 		 :publishing-directory "/ssh:user@host:~/html/notebook/"
+;; 		 :publishing-function org-html-publish-to-html
+;; 		 :exclude "PrivatePage.org" ;; regexp
+;; 		 :headline-levels 3
+;; 		 :section-numbers nil
+;; 		 :with-toc nil
+;; 		 :html-head "<link rel=\"stylesheet\"href=\"../other/mystyle.css\" type=\"text/css\"/>"
+;; 		 :html-preamble t)
+;; 		("images"
+;; 		 :base-directory "~/images/"
+;; 		 :base-extension "jpg\\|gif\\|png"
+;; 		 :publishing-directory "/ssh:user@host:~/html/images/"
+;; 		 :publishing-function org-publish-attachment)
+;; 		("other"
+;; 		 :base-directory "~/other/"
+;; 		 :base-extension "css\\|el"
+;; 		 :publishing-directory "/ssh:user@host:~/html/other/"
+;; 		 :publishing-function org-publish-attachment)
+;; 		("website" :components ("orgfiles" "images" "other"))))
 
 
 (provide 'init-org-mode)
